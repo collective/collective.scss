93743286a5114c9f952a1d366287d2e87b3bce66
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-10-31 10:53:55 +0100

    initial commit (zopeskel)

---
+++ collective/scss/interfaces/__init__.py
@@ -0,0 +1,2 @@
+from scsslayerlayer import ISCSSLayer
+
---
+++ collective/scss/interfaces/scsslayerlayer.py
@@ -0,0 +1,8 @@
+from zope.interface import Interface
+# -*- Additional Imports Here -*-
+
+
+class ISCSSLayer(Interface):
+    """ A layer specific to this product. 
+        Is registered using browserlayer.xml
+    """
